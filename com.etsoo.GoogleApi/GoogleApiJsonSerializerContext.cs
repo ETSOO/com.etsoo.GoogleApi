cb38a053aa8da1c7056216c31de7cf4683c71e22
Garry Xiao <xz@etsoo.com>
2024-02-01 20:22:03 +1300

    Upgrade dependencies

--- com.etsoo.GoogleApi/GoogleApiJsonSerializerContext.cs
+++ com.etsoo.GoogleApi/GoogleApiJsonSerializerContext.cs
@@ -6,7 +6,7 @@ namespace com.etsoo.GoogleApi
     /// Google API JSON serializer context
     /// 谷歌API JSON序列化上下文
     /// </summary>
-    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase, PropertyNameCaseInsensitive = true)]
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase, DictionaryKeyPolicy = JsonKnownNamingPolicy.CamelCase, PropertyNameCaseInsensitive = true)]
     [JsonSerializable(typeof(Cloud.RQ.TranslateTextRQ))]
     [JsonSerializable(typeof(Maps.Place.RQ.MapBaseRQ))]
     [JsonSerializable(typeof(Maps.Place.BaseResponse))]
@@ -18,7 +18,7 @@ namespace com.etsoo.GoogleApi
     /// Google API call JSON serializer context
     /// 谷歌API调用JSON序列化上下文
     /// </summary>
-    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.SnakeCaseLower, PropertyNameCaseInsensitive = true)]
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.SnakeCaseLower, DictionaryKeyPolicy = JsonKnownNamingPolicy.SnakeCaseLower, PropertyNameCaseInsensitive = true)]
     [JsonSerializable(typeof(Maps.Place.BaseResponse))]
     internal partial class GoogleApiCallJsonSerializerContext : JsonSerializerContext
     {
