15ce1d0dd5946398fae455587a6c6201c436a0de
Garry Xiao <xz@etsoo.com>
2024-02-01 18:01:54 +1300

    Change GoogleApiJsonSerializerContext naming policy

--- com.etsoo.GoogleApi/GoogleApiJsonSerializerContext.cs
+++ com.etsoo.GoogleApi/GoogleApiJsonSerializerContext.cs
@@ -6,6 +6,7 @@ namespace com.etsoo.GoogleApi
     /// Google API JSON serializer context
     /// 谷歌API JSON序列化上下文
     /// </summary>
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase, PropertyNameCaseInsensitive = true)]
     [JsonSerializable(typeof(Cloud.RQ.TranslateTextRQ))]
     [JsonSerializable(typeof(Maps.Place.RQ.MapBaseRQ))]
     [JsonSerializable(typeof(Maps.Place.BaseResponse))]
@@ -17,7 +18,7 @@ namespace com.etsoo.GoogleApi
     /// Google API call JSON serializer context
     /// 谷歌API调用JSON序列化上下文
     /// </summary>
-    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.SnakeCaseLower)]
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.SnakeCaseLower, PropertyNameCaseInsensitive = true)]
     [JsonSerializable(typeof(Maps.Place.BaseResponse))]
     internal partial class GoogleApiCallJsonSerializerContext : JsonSerializerContext
     {
