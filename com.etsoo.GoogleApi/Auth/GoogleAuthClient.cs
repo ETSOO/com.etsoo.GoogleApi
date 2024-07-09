70cf6b11dc956bb070dcf493c9f931ca0c1c6bb9
Garry Xiao <xz@etsoo.com>
2024-07-09 18:05:47 +1200

    Upgrade dependencies

--- com.etsoo.GoogleApi/Auth/GoogleAuthClient.cs
+++ com.etsoo.GoogleApi/Auth/GoogleAuthClient.cs
@@ -183,9 +183,21 @@ namespace com.etsoo.GoogleApi.Auth
         /// <param name="request">Callback request</param>
         /// <param name="state">Request state</param>
         /// <returns>Action result & user information</returns>
-        public async ValueTask<(IActionResult result, AuthUserInfo? userInfo)> GetUserInfoAsync(HttpRequest request, string state)
+        public ValueTask<(IActionResult result, AuthUserInfo? userInfo)> GetUserInfoAsync(HttpRequest request, string state)
         {
-            var (result, tokenData) = await ValidateAuthAsync(request, state);
+            return GetUserInfoAsync(request, s => s == state);
+        }
+
+        /// <summary>
+        /// Get user info from callback request
+        /// 从回调请求获取用户信息
+        /// </summary>
+        /// <param name="request">Callback request</param>
+        /// <param name="stateCallback">Callback to verify request state</param>
+        /// <returns>Action result & user information</returns>
+        public async ValueTask<(IActionResult result, AuthUserInfo? userInfo)> GetUserInfoAsync(HttpRequest request, Func<string, bool> stateCallback)
+        {
+            var (result, tokenData) = await ValidateAuthAsync(request, stateCallback);
             AuthUserInfo? userInfo = null;
             if (result.Ok && tokenData != null)
             {
@@ -221,9 +233,9 @@ namespace com.etsoo.GoogleApi.Auth
         /// 验证认证回调
         /// </summary>
         /// <param name="request">Callback request</param>
-        /// <param name="state">State</param>
+        /// <param name="stateCallback">Callback to verify request state</param>
         /// <returns>Action result & Token data</returns>
-        public async Task<(IActionResult result, GoogleTokenData? tokenData)> ValidateAuthAsync(HttpRequest request, string state)
+        public async Task<(IActionResult result, GoogleTokenData? tokenData)> ValidateAuthAsync(HttpRequest request, Func<string, bool> stateCallback)
         {
             IActionResult result;
             GoogleTokenData? tokenData = null;
@@ -239,7 +251,7 @@ namespace com.etsoo.GoogleApi.Auth
             else if (request.Query.TryGetValue("state", out var actualState) && request.Query.TryGetValue("code", out var codeSource))
             {
                 var code = codeSource.ToString();
-                if (!actualState.Equals(state))
+                if (!stateCallback(actualState.ToString()))
                 {
                     result = new ActionResult
                     {
--- com.etsoo.GoogleApi/Auth/IGoogleAuthClient.cs
+++ com.etsoo.GoogleApi/Auth/IGoogleAuthClient.cs
@@ -39,8 +39,8 @@ namespace com.etsoo.GoogleApi.Auth
         /// 验证认证回调
         /// </summary>
         /// <param name="request">Callback request</param>
-        /// <param name="state">State</param>
+        /// <param name="stateCallback">Callback to verify request state</param>
         /// <returns>Action result & Token data</returns>
-        Task<(IActionResult result, GoogleTokenData? tokenData)> ValidateAuthAsync(HttpRequest request, string state);
+        Task<(IActionResult result, GoogleTokenData? tokenData)> ValidateAuthAsync(HttpRequest request, Func<string, bool> stateCallback);
     }
 }
